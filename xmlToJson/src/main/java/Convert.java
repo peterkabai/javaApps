d5b0ea7cf7d54b5878732819b95183b5315a44fd
Peter <pkabai@csumb.edu>
2018-12-28 13:53:55 -0800

    Removes unused imports

--- xmlToJson/src/main/java/Convert.java
+++ xmlToJson/src/main/java/Convert.java
@@ -1,17 +1,10 @@
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.XML;
-
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Scanner;
 
 public class Convert {
 
