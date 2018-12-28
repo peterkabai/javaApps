33b3846a0082af3ec887efbc153377b5145b4797
Peter <pkabai@csumb.edu>
2018-12-28 13:50:01 -0800

    Saves the json string to a file

--- xmlToJson/src/main/java/Convert.java
+++ xmlToJson/src/main/java/Convert.java
@@ -5,6 +5,7 @@ import org.json.XML;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -13,20 +14,29 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Convert {
-
-
+    
     public static void main(String[] args) throws IOException {
 
-        String resources = System.getProperty("user.dir") + "/src/main/resources/";
-        String filename = "data.xml";
-        String text = new String(Files.readAllBytes(Paths.get(resources + filename)));
-
+        // pick the data and json settings
+        String inFile = "data.xml";
+        String outFile = "data.json";
         int indents = 4;
 
+        // gets the text from the input file
+        String resources = System.getProperty("user.dir") + "/src/main/resources/";
+        String text = new String(Files.readAllBytes(Paths.get(resources + inFile)));
+
         try {
-            JSONObject xmlJSONObj = XML.toJSONObject(text);
-            String jsonPrettyPrintString = xmlJSONObj.toString(indents);
-            System.out.println(jsonPrettyPrintString);
+            // convert to pretty json
+            JSONObject obj = XML.toJSONObject(text);
+            String json = obj.toString(indents);
+            System.out.println(json);
+
+            // save to json file
+            PrintWriter writer = new PrintWriter(resources + outFile, "UTF-8");
+            writer.println(json);
+            writer.close();
+
         } catch (JSONException je) {
             System.out.println(je.toString());
         }
