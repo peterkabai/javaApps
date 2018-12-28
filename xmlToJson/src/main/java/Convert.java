cc6c4e865c08c8fbc3bac1e75f07a613321e7d80
Peter <pkabai@csumb.edu>
2018-12-28 12:34:03 -0800

    Adds test txt file to read from

--- xmlToJson/src/main/java/Convert.java
+++ xmlToJson/src/main/java/Convert.java
@@ -2,11 +2,37 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.XML;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Convert {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
 
         String xml = "<?xml version=\"1.0\" ?><test attrib=\"moretest\">Turn this to JSON</test>";
+
+
+        String filename = "/Users/peter/Desktop/GitHub/javaApps/xmlToJson/src/main/resources/test.txt";
+        String resources = System.getProperty("user.dir") + "/src/main/resources/";
+        filename = "test.txt";
+
+
+
+        //URL url = Convert.class.getClassLoader().getResource("resources/test.txt");
+        //File file = new File(url);
+
+        String text = new String(Files.readAllBytes(Paths.get(resources + filename)));
+        System.out.println(text);
+
+
         int indents = 4;
 
         try {
